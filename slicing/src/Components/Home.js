55f3d2f722d43f85b0441c267cb55d2c87206c25
Utsav170 <stha24@gmail.com>
2020-05-08 13:33:01 +0545

    add routing

---
+++ slicing/src/Components/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+    render(){
+        console.log('l')
+        return(
+            <div>HOME</div>
+        )
+    }
+}
+
+export default Home
