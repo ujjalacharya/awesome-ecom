6517012ec014112ee7c5dd7e411caf56acce8285
Utsav170 <stha24@gmail.com>
2020-05-10 20:05:33 +0545

    add new product slider

--- slicing/src/Components/SliderHeader.js
+++ slicing/src/Components/SliderHeader.js
@@ -3,11 +3,10 @@ import React, { Component } from "react";
 class SliderHeader extends Component {
   render() {
     return (
-      <div className="slider-header">
-        <h2>Featured Products</h2>
+      <div className={"slider-header "+this.props.removePaddingTop} >
+        <h2>{this.props.headTitle}</h2>
         <div className="slider-detail">
-          Quicksand is a sans serif type family of three weights plus matching
-          obliques
+          {this.props.headDetails}
         </div>
       </div>
     );
--- slicing/src/Pages/Home.js
+++ slicing/src/Pages/Home.js
@@ -1,36 +1,46 @@
-import React, { Component } from 'react'
-import Header from '../Components/Header'
-import MainCarousel from '../Components/Carousel'
-import ProductSlider from '../Components/ProductSlider'
-import SliderHeader from '../Components/SliderHeader'
-import { Row, Col } from 'antd'
-import Popular from '../Components/Popular'
-import LatestSLider from '../Components/LatestSlider'
+import React, { Component } from "react";
+import Header from "../Components/Header";
+import MainCarousel from "../Components/Carousel";
+import ProductSlider from "../Components/ProductSlider";
+import SliderHeader from "../Components/SliderHeader";
+import { Row, Col } from "antd";
+import Popular from "../Components/Popular";
+import LatestSLider from "../Components/LatestSlider";
 
 class Home extends Component {
-    render(){
-        return(
-            <div className="wrapper">
-                <Header />
-                <div className="main-carousel">
-                    <MainCarousel />
-                </div>
-                <SliderHeader />
-                <ProductSlider />
-                <section className="latest-popular">
-                    <Row>
-                        <Col span={12}>
-                            <Popular />
-                        </Col>
-                        <Col span={12}>
-                            <LatestSLider />
-                        </Col>
-                    </Row>
-                </section>
-                Body
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="wrapper">
+        <Header />
+        <div className="main-carousel">
+          <MainCarousel />
+        </div>
+        <SliderHeader
+          headTitle="Featured Products"
+          headDetails="Quicksand is a sans serif type family of three weights plus matching
+          obliques"
+        />
+        <ProductSlider />
+        <section className="latest-popular">
+          <Row>
+            <Col span={12}>
+              <Popular />
+            </Col>
+            <Col span={12}>
+              <LatestSLider />
+            </Col>
+          </Row>
+        </section>
+        <SliderHeader
+          headTitle="Trending Products"
+          headDetails="Quicksand is a sans serif type family of three weights plus matching obliques"
+          removePaddingTop="paddingTopZero"
+        />
+        <ProductSlider />
+        Body
+      </div>
+    );
+  }
 }
 
-export default Home
+export default Home;
