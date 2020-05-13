555ec5de4195f539d79e0401719e9072d2d3cfcd
Ujjal Acharya <ujjal.acharya@amniltech.com>
2020-05-14 00:17:14 +0545

    make private route with cookie working

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,14 +1,46 @@
-import Head from 'next/head'
+import React from 'react';
+import Link from 'next/link.js';
+import axios from 'axios';
+import { Cookies } from 'react-cookie';
 
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+const serverUrl = 'http://localhost:3001';
 
-     <h1>Starter</h1>
-    </div>
-  )
+// set up cookies
+const cookies = new Cookies();
+class Index extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      token: cookies.get('token') || null
+    }
+  }
+
+  onLoginClick = async () => {
+    console.log("Login called");
+    const response = await axios.get(serverUrl + '/api/login')
+    const token = response.data.token;
+    cookies.set('token', token);
+    this.setState({
+      token: token
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Main page</h2>
+        <br></br>
+        <button onClick={() => this.onLoginClick()}>Get Token</button>
+        <br></br>
+        <p>Token: {this.state.token}</p>
+        <br></br>
+        <Link href="/secret">
+          <a>Secret page</a>
+        </Link>
+      </div >
+    )
+  }
 }
+
+export default Index;
