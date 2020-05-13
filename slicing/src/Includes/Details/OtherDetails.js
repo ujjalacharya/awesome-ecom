4a18f9aaa6006350dea5418ac37380dbf850b4a6
Utsav170 <stha24@gmail.com>
2020-05-13 15:14:32 +0545

    add additional info in product detauls

--- slicing/src/Includes/Details/OtherDetails.js
+++ slicing/src/Includes/Details/OtherDetails.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { Tabs } from "antd";
+import QA from "./Includes/Q&A";
+import AdditionalInformation from "./Includes/AdditionalInfo";
 
 const { TabPane } = Tabs;
 
@@ -13,112 +15,21 @@ class OtherDetails extends Component {
       <div className="other-details">
         <Tabs defaultActiveKey="1" onChange={this.callback}>
           <TabPane tab="Q/A" key="1">
-            <div className="q-a-tab">
-              <h3>Questions about this product (5)</h3>
-              <div className="not-logged-in">
-                <a>Login</a> or <a>Register</a> to ask questions
-              </div>
-              <div className="qns-ans-section">
-                <div className="title">
-                  Questions answered by The Fashionista
-                </div>
-                <div className="qns-details">
-                  <div className="qns">
-                    <span className="q-icon">
-                      Q<span class="arrow-bottom"></span>
-                    </span>
-                    <span className="q-title">
-                      <span className="q-text">discount xaina??</span>
-                      <div className="user">Bibek L. - 37 minutes ago</div>
-                    </span>
-                  </div>
-                  <div className="ans">
-                    <span className="q-icon a-btn">
-                      A<span class="arrow-bottom"></span>
-                    </span>
-                    <span className="q-title">
-                      <span className="q-text">Already discounted rate</span>
-                      <div className="user">
-                        The Fashionista - 12 minutes ago
-                      </div>
-                    </span>
-                  </div>
-
-                  <div className="qns">
-                    <span className="q-icon">
-                      Q<span class="arrow-bottom"></span>
-                    </span>
-                    <span className="q-title">
-                      <span className="q-text">discount xaina??</span>
-                      <div className="user">Bibek L. - 37 minutes ago</div>
-                    </span>
-                  </div>
-                  <div className="ans">
-                    <span className="q-icon a-btn">
-                      A<span class="arrow-bottom"></span>
-                    </span>
-                    <span className="q-title">
-                      <span className="q-text">Already discounted rate</span>
-                      <div className="user">
-                        The Fashionista - 12 minutes ago
-                      </div>
-                    </span>
-                  </div>
-
-                  <div className="qns">
-                    <span className="q-icon">
-                      Q<span class="arrow-bottom"></span>
-                    </span>
-                    <span className="q-title">
-                      <span className="q-text">comes with charger?</span>
-                      <div className="user">Jayesh R. - 28 Oct 2019</div>
-                    </span>
-                  </div>
-                  <div className="ans">
-                    <span className="q-icon a-btn">
-                      A<span class="arrow-bottom"></span>
-                    </span>
-                    <span className="q-title">
-                      <span className="q-text">
-                        Dear customer,you can charge it with your mobile charger
-                      </span>
-                      <div className="user">
-                        Titan Official - answered within 2 days
-                      </div>
-                    </span>
-                  </div>
-
-                  <div className="qns">
-                    <span className="q-icon">
-                      Q<span class="arrow-bottom"></span>
-                    </span>
-                    <span className="q-title">
-                      <span className="q-text">can it be returned??</span>
-                      <div className="user">Pancha R. - 13 Nov 2019</div>
-                    </span>
-                  </div>
-                  <div className="ans">
-                    <span className="q-icon a-btn">
-                      A<span class="arrow-bottom"></span>
-                    </span>
-                    <span className="q-title">
-                      <span className="q-text">
-                        Why sir didn't you liked it
-                      </span>
-                      <div className="user">
-                        Titan Official - answered within 5 minutes
-                      </div>
-                    </span>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <QA />
           </TabPane>
           <TabPane tab="Description" key="2">
-            Content of Tab Pane 2
+            <div className="desc-tab">
+              <div className="title">Description</div>
+              <ul>
+                <li>Lorem ipsum dolor sit amet</li>
+                <li>consectetur adipiscing elit</li>
+                <li>Lorem ipsum dolor sit amet</li>
+                <li>Nam non enim mollis</li>
+              </ul>
+            </div>
           </TabPane>
           <TabPane tab="Additional Information" key="3">
-            Content of Tab Pane 3
+            <AdditionalInformation />
           </TabPane>
           <TabPane tab="Reviews" key="4">
             Content of Tab Pane 4
