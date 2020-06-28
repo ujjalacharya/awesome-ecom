c2842b1b115680764b3fc93a2a63e61d981fa29c
Utsav170 <stha24@gmail.com>
2020-06-28 22:26:29 +0545

    add search pag

--- client/redux/reducers/listingReducer.js
+++ client/redux/reducers/listingReducer.js
@@ -1,13 +1,16 @@
-import { LATEST_PRODUCTS, PRODUCT_DETAILS, GET_BRANDS } from "../types";
+import { GET_BRANDS, SEARCH_PRODUCTS } from "../types";
 
 const initialState = {
-  getAllBrands: null
+  getAllBrands: null,
+  getSearchData: null
 };
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case GET_BRANDS:
       return { getAllBrands: action.payload };
+      case SEARCH_PRODUCTS:
+      return { getSearchData: action.payload };
     default:
       return state;
   }
