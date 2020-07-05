18d0453280942bf3b9d97db185a584c0161c6dbe
Ujjal Acharya <ujjal.acharya@amniltech.com>
2020-07-05 09:17:04 +0545

    remove unwanted packages and lines of code

--- client/pages/api/token.js
+++
@@ -1,8 +0,0 @@
-export default (req, res) => {
-  if (req.method === "POST") {
-    res.status(200).json({
-      token:
-        "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI1ZWJjMTEyZWYzYWVmMTBhMjZmMmEyMTciLCJpYXQiOjE1ODk0NDU5NDkwNDN9.GjnCUrdFag5kblQjGXzAz7r5qfFNQpuzQ-SjJd8pupM",
-    });
-  }
-};
