2d8250b6dc2bfc30d46d5731769d80e0326c3ab2
Ujjal Acharya <ujjal.acharya@amniltech.com>
2020-07-08 01:13:34 +0545

    mobile/add conditional back button on cart screen depending on the type

--- app/src/screens/CartScreen.js
+++ app/src/screens/CartScreen.js
@@ -1,10 +1,29 @@
 import React, { Component } from "react";
 import { Text, View } from "react-native";
 
+import { Appbar } from "react-native-paper";
+import { AntDesign } from "@expo/vector-icons";
+import Constants from "../constants/Constants";
+
 export class CartScreen extends Component {
+  _goBack = () => {
+    this.props.navigation.pop();
+  };
+
   render() {
+    const isCartStack = this.props.route.name === "CartStack";
     return (
       <View>
+        <Appbar.Header statusBarHeight={0}>
+          {isCartStack && (
+            <Appbar.BackAction
+              color={Constants.headerTintColor}
+              onPress={this._goBack}
+            />
+          )}
+
+          <Appbar.Content title="Cart" color={Constants.headerTintColor} />
+        </Appbar.Header>
         <Text> Cart Screen </Text>
         <Text> Cart Screen </Text>
         <Text> Cart Screen </Text>
