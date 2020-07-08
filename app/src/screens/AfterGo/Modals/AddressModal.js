8af015bbdf2056a6c97a896679e6a8e9f23850c7
Ujjal Acharya <ujjal.acharya@amniltech.com>
2020-07-08 15:40:36 +0545

    mobile/show dummy modal for sort on search  product

---
+++ app/src/screens/AfterGo/Modals/AddressModal.js
@@ -0,0 +1,75 @@
+import React, { Component } from "react";
+import { Text, View, Modal, StyleSheet } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
+
+export class AddressModal extends Component {
+  render() {
+    return (
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={this.props.showModal}
+        onRequestClose={this.props.handleModalVisibility}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Ionicons
+              name="ios-close-circle"
+              size={30}
+              color="red"
+              style={{ ...styles.openButton }}
+              onPress={this.props.handleModalVisibility}
+            />
+            <Text>{"Smth smth"}</Text>
+            <Text>{"Smth smth"}</Text>
+            <Text>{"Smth smth"}</Text>
+          </View>
+        </View>
+      </Modal>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+  modalView: {
+    position: "absolute",
+    bottom: 0,
+    height: "30%",
+    width: "100%",
+    margin: 20,
+    backgroundColor: "#DCDCDC",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  openButton: {
+    position: "absolute",
+    right: 15,
+    marginTop: 10,
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
+  },
+});
+
+export default AddressModal;
--- app/src/screens/AfterGo/ProductListScreen.js
+++ app/src/screens/AfterGo/ProductListScreen.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Text, View } from "react-native";
-import { Appbar } from "react-native-paper";
+import { Appbar, Button } from "react-native-paper";
 
 import { ScrollView } from "react-native-gesture-handler";
 import { AntDesign } from "@expo/vector-icons";
@@ -9,66 +9,85 @@ import { connect } from "react-redux";
 import SearchedSingleProduct from "../../components/SearchedSingleProduct";
 import { productData } from "../../utils/mock";
 import Constants from "../../constants/Constants";
+import AddressModal from "./Modals/AddressModal";
 
 export class ProductListScreen extends Component {
   state = {
     progressBar: 0,
     intervalId: "",
     progressColor: "#b2cede",
+    showModal: false,
   };
 
   _goBack = () => {
     this.props.navigation.pop();
   };
 
+  handleModalVisibility = () => {
+    this.setState({
+      showModal: !this.state.showModal,
+    });
+  };
+
   render() {
     return (
-      <View>
-        <Appbar.Header statusBarHeight={0}>
-          <Appbar.BackAction
-            color={Constants.headerTintColor}
-            onPress={this._goBack}
-          />
-          <Appbar.Content title="Search" color={Constants.headerTintColor} />
-          <Appbar.Action
-            color={Constants.headerTintColor}
-            icon={() => (
-              <AntDesign
-                name="search1"
-                size={20}
-                color={Constants.headerTintColor}
-              />
-            )}
-            onPress={() => this.props.navigation.pop()}
-          />
-          <Appbar.Action
-            color={Constants.headerTintColor}
-            icon="heart"
-            onPress={() => this.props.navigation.navigate("WishList")}
-          />
-          <Appbar.Action
-            color={Constants.headerTintColor}
-            icon={() => (
-              <AntDesign
-                name="shoppingcart"
-                size={20}
-                color={Constants.headerTintColor}
-              />
-            )}
-            onPress={() => this.props.navigation.navigate("CartStack")}
-          />
-        </Appbar.Header>
-        {/* <ActivityIndicator animating={true} color={Constants.tintColor}/> */}
-        <ScrollView
-          style={{ height: "100%", margin: 15 }}
-          showsVerticalScrollIndicator={false}
-          showsHorizontalScrollIndicator={false}
-        >
-          {productData.map((bus, i) => (
-            <SearchedSingleProduct bus={bus} key={i} />
-          ))}
-        </ScrollView>
-      </View>
+      <>
+        <AddressModal
+          handleModalVisibility={this.handleModalVisibility}
+          showModal={this.state.showModal}
+          addressState={this.state.addressState}
+          handleFromTo={this.handleFromTo}
+        />
+        <View>
+          <Appbar.Header statusBarHeight={0}>
+            <Appbar.BackAction
+              color={Constants.headerTintColor}
+              onPress={this._goBack}
+            />
+            <Appbar.Content title="Search" color={Constants.headerTintColor} />
+            <Appbar.Action
+              color={Constants.headerTintColor}
+              icon={() => (
+                <AntDesign
+                  name="search1"
+                  size={20}
+                  color={Constants.headerTintColor}
+                />
+              )}
+              onPress={() => this.props.navigation.pop()}
+            />
+            <Appbar.Action
+              color={Constants.headerTintColor}
+              icon="heart"
+              onPress={() => this.props.navigation.navigate("WishList")}
+            />
+            <Appbar.Action
+              color={Constants.headerTintColor}
+              icon={() => (
+                <AntDesign
+                  name="shoppingcart"
+                  size={20}
+                  color={Constants.headerTintColor}
+                />
+              )}
+              onPress={() => this.props.navigation.navigate("CartStack")}
+            />
+          </Appbar.Header>
+          {/* <ActivityIndicator animating={true} color={Constants.tintColor}/> */}
+          <ScrollView
+            style={{ height: "100%", margin: 15, backgroundColor: "green" }}
+            showsVerticalScrollIndicator={false}
+            showsHorizontalScrollIndicator={false}
+          >
+            <Button onPress={this.handleModalVisibility}>
+              {"Click me to open modal"}
+            </Button>
+            {productData.map((bus, i) => (
+              <SearchedSingleProduct bus={bus} key={i} />
+            ))}
+          </ScrollView>
+        </View>
+      </>
     );
   }
 }
