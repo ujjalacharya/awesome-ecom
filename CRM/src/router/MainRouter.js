502be1b705e8169d5fb74da3b41846f5034e0d7b
Ujjal Acharya <ujjal.acharya@amniltech.com>
2020-07-14 17:30:25 +0545

    CRM/add conditional singin component

--- CRM/src/router/MainRouter.js
+++ CRM/src/router/MainRouter.js
@@ -1,12 +1,28 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from "../components/pages/Home";
-// import Home from "../components/pages/Signin";
+import { connect } from "react-redux";
+import { signIn } from "../redux/actions/user_actions";
+import Signin from "../components/pages/Signin";
 
-const MainRouter = () => (
- <Switch>
-   <Route path="/" exact component={Home} />
- </Switch>
-);
+const MainRouter = (props) => {
+  if (props.isAuthenticated) {
+    return (
+      <Switch>
+        <Route path="/" exact component={Home} />
+      </Switch>
+    );
+  } else {
+    return (
+      <Switch>
+        <Route path="/" exact component={Signin} />
+      </Switch>
+    );
+  }
+};
 
-export default MainRouter;
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.User.auth.isAuth,
+});
+
+export default connect(mapStateToProps, { signIn })(MainRouter);
