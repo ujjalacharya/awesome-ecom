d7f76a09715719ae8b2652dd5731163e375108a2
Ujjal Acharya <ujjal.acharya@amniltech.com>
2020-07-14 16:32:19 +0545

    CRM/add signin page

--- CRM/src/router/MainRouter.js
+++ CRM/src/router/MainRouter.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from "../components/pages/Home";
+// import Home from "../components/pages/Signin";
 
 const MainRouter = () => (
  <Switch>
