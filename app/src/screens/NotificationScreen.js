17052a027ca69658697ca040c3b76d559740d413
Ujjal Acharya <ujjal.acharya@amniltech.com>
2020-07-20 18:23:40 +0545

    mobile/add dummy nonotifcation image on notification screen

--- app/src/screens/NotificationScreen.js
+++ app/src/screens/NotificationScreen.js
@@ -1,24 +1,31 @@
 import React, { Component } from "react";
-import { Text, View } from "react-native";
+import { Text, View, Image } from "react-native";
 import { Appbar } from "react-native-paper";
 import Constants from "../constants/Constants";
 
 export class Notification extends Component {
   render() {
     return (
-      <View>
+      <View style={{ flex: 1 }}>
         <Appbar.Header statusBarHeight={0}>
           <Appbar.Content
             title="Notification"
             color={Constants.headerTintColor}
           />
         </Appbar.Header>
-        <Text> Notification Screen </Text>
-        <Text> Notification Screen </Text>
-        <Text> Notification Screen </Text>
-        <Text> Notification Screen </Text>
-        <Text> Notification Screen </Text>
-        <Text> Notification Screen </Text>
+        <View
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        >
+          <Image
+            source={require("../../assets/nonotification.png")}
+            style={{
+              flex: 1,
+              width: 50,
+              height: 50,
+              resizeMode: "contain",
+            }}
+          />
+        </View>
       </View>
     );
   }
