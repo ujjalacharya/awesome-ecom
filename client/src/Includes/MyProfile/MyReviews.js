72f4d46664b72067db8bc70c3450a56a46749642
Utsav170 <stha24@gmail.com>
2020-07-23 13:26:53 +0545

    view review in profile page

--- client/src/Includes/MyProfile/MyReviews.js
+++ client/src/Includes/MyProfile/MyReviews.js
@@ -1,13 +1,47 @@
 import React, { Component } from "react";
+import { withRouter } from "next/router";
+import actions from "../../../redux/actions";
+import { connect } from "react-redux";
 
 class MyReviews extends Component {
   render() {
+    console.log(this.props);
+    let { myReviews } = this.props.user.myReviews;
     return (
       <div className="my-reviews">
-          You have no reviews
+        <h3>My Reviews</h3>
+        {myReviews?.map((review, i) => {
+          return (
+            <div key={i} className="shop-review">
+              <div className="shop-name">{review.product.soldBy.shopName}</div>
+              <div className="product-name">{review.product.name}</div>
+              <div className="ratings">
+                {Array(review.star)
+                  .fill(0)
+                  .map((num, i) => {
+                    return (
+                      <i className="fa fa-star" aria-hidden="true" key={i}></i>
+                    );
+                  })}
+                {Array(5 - review.star)
+                  .fill(0)
+                  .map((num, k) => {
+                    return (
+                      <i
+                        className="fa fa-star fade-star"
+                        aria-hidden="true"
+                        key={k}
+                      ></i>
+                    );
+                  })}
+              </div>
+                <div className="review-cmt">{review.comment}</div>
+            </div>
+          );
+        })}
       </div>
     );
   }
 }
 
-export default MyReviews;
+export default connect((state) => state, actions)(withRouter(MyReviews));
