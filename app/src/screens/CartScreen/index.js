43698af4dcf6236da62155baf529be2f7633cb74
Ujjal Acharya <ujjal.acharya@amniltech.com>
2020-07-26 10:35:29 +0545

    add footer to cart screen

--- app/src/screens/CartScreen/index.js
+++ app/src/screens/CartScreen/index.js
@@ -5,7 +5,14 @@ import {
   ScrollView,
 } from "react-native-gesture-handler";
 
-import { Avatar, Card, Title, Paragraph, Appbar } from "react-native-paper";
+import {
+  Avatar,
+  Card,
+  Title,
+  Paragraph,
+  Appbar,
+  Button,
+} from "react-native-paper";
 import { View, Text, Image, StyleSheet } from "react-native";
 
 import Constants from "../../constants/Constants";
@@ -21,65 +28,84 @@ export class CartScreen extends Component {
   render() {
     const isCartStack = this.props.route.name === "CartStack";
     return (
-      <ScrollView
-        contentContainerStyle={{ flexGrow: 1 }}
-        stickyHeaderIndices={[1]}
-        showsVerticalScrollIndicator={false}
-      >
-        <View style={{ height: 50 }}>
-          <Appbar.Header statusBarHeight={0}>
-            {isCartStack && (
-              <Appbar.BackAction
-                color={Constants.headerTintColor}
-                onPress={this._goBack}
-              />
-            )}
+      <>
+        <ScrollView
+          contentContainerStyle={{ flexGrow: 1 }}
+          // stickyHeaderIndices={[1]}
+          showsVerticalScrollIndicator={false}
+        >
+          <View style={{ height: 50 }}>
+            <Appbar.Header statusBarHeight={0}>
+              {isCartStack && (
+                <Appbar.BackAction
+                  color={Constants.headerTintColor}
+                  onPress={this._goBack}
+                />
+              )}
 
-            <Appbar.Content title="Cart" color={Constants.headerTintColor} />
-          </Appbar.Header>
-        </View>
+              <Appbar.Content title="Cart" color={Constants.headerTintColor} />
+            </Appbar.Header>
+          </View>
 
-        <TouchableWithoutFeedback>
-          <Card
-            // onPress={() => props.navigation.navigate("Detail")}
-            style={{ marginBottom: 5 }}
-          >
-            <Card.Content>
-              <View style={{ flex: 1, flexDirection: "row", marginTop: 5 }}>
-                <View style={{ flex: 1.5 }}>
-                  <Image
-                    style={styles.tinyLogo}
-                    source={{ uri: product.image }}
-                  />
-                </View>
-                <View style={{ flex: 2}}>
-                  <>
-                    <Title>{product.title}</Title>
-                    <Paragraph>{product.price}</Paragraph>
-                    <Avatar.Text
-                      size={24}
-                      label="4/5 stars"
-                      color={Constants.headerTintColor}
-                      backgroundColor="green"
-                      width={90}
-                      style={{ marginTop: 10 }}
+          {[0, 0, 0, 0, 0, 0, 0].map((data, i) => (
+            <TouchableWithoutFeedback key={i}>
+              <Card
+                // onPress={() => props.navigation.navigate("Detail")}
+                style={{ marginBottom: 5 }}
+              >
+                <Card.Content>
+                  <View style={{ flex: 1, flexDirection: "row", marginTop: 5 }}>
+                    <View style={{ flex: 1.5 }}>
+                      <Image
+                        style={styles.tinyLogo}
+                        source={{ uri: product.image }}
                       />
-                   
-                  </>
-                </View>
-              </View>
-            </Card.Content>
-            {/* <Card.Actions
-              style={{
-                backgroundColor: Constants.cardColor,
-                marginTop: 5,
-              }}
-            >
-              {renderActionButtonComponent(props.type, props.product)}
-            </Card.Actions> */}
-          </Card>
-        </TouchableWithoutFeedback>
-      </ScrollView>
+                    </View>
+                    <View style={{ flex: 2 }}>
+                      <>
+                        <Title>{product.title}</Title>
+                        <Paragraph>{product.price}</Paragraph>
+                        <Avatar.Text
+                          size={24}
+                          label="4/5 stars"
+                          color={Constants.headerTintColor}
+                          backgroundColor="green"
+                          width={90}
+                          style={{ marginTop: 10 }}
+                        />
+                      </>
+                    </View>
+                  </View>
+                </Card.Content>
+              </Card>
+            </TouchableWithoutFeedback>
+          ))}
+        </ScrollView>
+
+        <View
+          style={{ backgroundColor: Constants.headerTintColor, height: 50 }}
+        >
+          <View style={{ flex: 1, flexDirection: "row" }}>
+            <View style={{ flex: 0.3 }}></View>
+            <View style={{ flex: 0.3 }}></View>
+            <View style={{ flex: 0.4 }}>
+              <Button
+                style={{
+                  flex: 1,
+                  backgroundColor: "orange",
+                  justifyContent: "center",
+                  margin: 5,
+                  borderRadius: 5
+                }}
+                labelStyle={{ color: "white" }}
+                // onPress={this.setVisible}
+              >
+                Check Out
+              </Button>
+            </View>
+          </View>
+        </View>
+      </>
     );
   }
 }
