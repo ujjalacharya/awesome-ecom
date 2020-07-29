3d8a36f87915bdc151373bc6c495476e843fae29
Ujjal Acharya <ujjal.acharya@amniltech.com>
2020-07-29 16:00:08 +0545

    mobile/add back button to checkout

--- app/src/screens/CheckOut/index.js
+++ app/src/screens/CheckOut/index.js
@@ -4,14 +4,20 @@ import { Appbar } from "react-native-paper";
 import Constants from "../../constants/Constants";
 
 export class CheckOut extends Component {
+  _goBack = () => {
+    this.props.navigation.pop();
+  };
+
   render() {
     return (
       <View style={{ flex: 1 }}>
         <Appbar.Header statusBarHeight={0}>
-          <Appbar.Content
-            title="Checkout"
+          <Appbar.BackAction
             color={Constants.headerTintColor}
+            onPress={this._goBack}
           />
+
+          <Appbar.Content title="Checkout" color={Constants.headerTintColor} />
         </Appbar.Header>
         <View
           style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
