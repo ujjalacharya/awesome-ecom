417b4e51018c089bb8bb8f4cad981c149e9771e9
Utsav170 <stha24@gmail.com>
2020-08-02 21:23:59 +0545

    add menu dynamic in mobile

--- client/src/Components/Includes/MenuDrawer.js
+++ client/src/Components/Includes/MenuDrawer.js
@@ -1,20 +1,21 @@
 import React, { Component } from "react";
-import { Drawer, Button, Radio, Space } from "antd";
+import { Drawer, Collapse } from "antd";
+import _ from 'lodash'
 
 class MenuDrawer extends Component {
   state = { placement: "left" };
 
-//   showDrawer = () => {
-//     this.setState({
-//       visible: true,
-//     });
-//   };
+  //   showDrawer = () => {
+  //     this.setState({
+  //       visible: true,
+  //     });
+  //   };
 
-//   onClose = () => {
-//     this.setState({
-//       visible: false,
-//     });
-//   };
+  //   onClose = () => {
+  //     this.setState({
+  //       visible: false,
+  //     });
+  //   };
 
   onChange = (e) => {
     this.setState({
@@ -22,21 +23,103 @@ class MenuDrawer extends Component {
     });
   };
 
+  callback = (key) => {
+    console.log(key);
+  };
+
   render() {
     const { placement } = this.state;
+    let { parentCate } = this.props;
+
+    const text = `
+  A dog is a type of domesticated animal.
+  Known for its loyalty and faithfulness,
+  it can be found as a welcome guest in many households across the world.
+`;
     return (
       <>
         <Drawer
-          title="Basic Drawer"
+          title="Menu"
           placement={placement}
           closable={false}
           onClose={this.props.onCloseDrawer}
           visible={this.props.showDrawer}
           key={placement}
+          className="mobile-menu-drawer"
         >
-          <p>Some contents...</p>
-          <p>Some contents...</p>
-          <p>Some contents...</p>
+          <div className="menu-list alldepart">
+            <ul className="category">
+              {parentCate?.map((cate, i) => {
+                return (
+                  <li key={i}>
+                    <div className="title main-title">
+                      <span>{_.capitalize(cate.displayName)}</span>
+                      <span className="title-icon">
+                        {cate.childCate.length > 0 && (
+                          <i
+                            className="fa fa-angle-down"
+                            aria-hidden="true"
+                          ></i>
+                        )}
+                      </span>
+                    </div>
+                    {cate.childCate.length > 0 && (
+                      <ul className="sub-category">
+                        {cate.childCate.map((subCate, i) => {
+                          return (
+                            <li
+                              key={i}
+                              onClick={(e) =>
+                                this.searchProducts(
+                                  e,
+                                  subCate.slug,
+                                  subCate._id
+                                )
+                              }
+                            >
+                              <div className="title sub-title">
+                                <span>{_.capitalize(subCate.displayName)}</span>
+                                <span className="sub-title-icon">
+                                  {subCate.childCate.length > 0 && (
+                                    <i
+                                      className="fa fa-angle-down"
+                                      aria-hidden="true"
+                                    ></i>
+                                  )}
+                                </span>
+                              </div>
+                              {subCate.childCate.length > 0 && (
+                                <ul className="sub-category next-sub">
+                                  {subCate.childCate.map((newSubCate, i) => {
+                                    return (
+                                      <li
+                                        key={i}
+                                        onClick={(e) =>
+                                          this.searchProducts(
+                                            e,
+                                            newSubCate.slug,
+                                            newSubCate._id
+                                          )
+                                        }
+                                      >
+                                        <div className="title sub-sub-title">
+                                          <span>{newSubCate.displayName}</span>
+                                        </div>
+                                      </li>
+                                    );
+                                  })}
+                                </ul>
+                              )}
+                            </li>
+                          );
+                        })}
+                      </ul>
+                    )}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
         </Drawer>
       </>
     );
--- client/src/Components/MobileHeader.js
+++ client/src/Components/MobileHeader.js
@@ -1,8 +1,54 @@
 import React, { Component } from "react";
 import MenuDrawer from "./Includes/MenuDrawer";
+import { connect } from "react-redux";
+import actions from "../../redux/actions";
+import { withRouter } from "next/router";
+import { getChildCategories, getUserInfo } from "../../utils/common";
 
 class MobileHeader extends Component {
   state = { visible: false };
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.authentication.token !== nextProps.authentication.token) {
+      let userInfo = [];
+      if (nextProps.authentication.token) {
+        userInfo = getUserInfo(loginToken);
+      }
+      this.setState({
+        loginToken: nextProps.authentication.token,
+        userInfo,
+      });
+    }
+
+    if (
+      this.props.menu.menuCategories !== nextProps.menu.menuCategories &&
+      nextProps.menu.menuCategories
+    ) {
+      let parentCategory = [];
+
+      let parentCate = [];
+      let {
+        menuCategories: { categories },
+      } = nextProps.menu;
+      categories.map((cate) => {
+        if (cate.parent === undefined) {
+          parentCategory.push(cate);
+        }
+      });
+
+      let allCates = getChildCategories(categories, parentCategory);
+
+      allCates.map((newChild) => {
+        let newallCates = getChildCategories(categories, newChild.childCate);
+        let parentCateEle = { ...newChild, childCate: newallCates };
+        parentCate.push(parentCateEle);
+      });
+
+      this.setState({
+        parentCate,
+      });
+    }
+  }
 
   showDrawer = () => {
     this.setState({
@@ -17,6 +63,8 @@ class MobileHeader extends Component {
   };
 
   render() {
+    let { parentCate } = this.state
+
     return (
       <div className="mobile-header">
         <div className="menu-logo">
@@ -30,10 +78,14 @@ class MobileHeader extends Component {
         <div className="search-mob">
           <i class="fa fa-search" aria-hidden="true"></i>
         </div>
-        <MenuDrawer showDrawer={this.state.visible} onCloseDrawer={this.onCloseDrawer} />
+        <MenuDrawer
+          showDrawer={this.state.visible}
+          onCloseDrawer={this.onCloseDrawer}
+          parentCate = {parentCate}
+        />
       </div>
     );
   }
 }
 
-export default MobileHeader;
+export default connect((state) => state, actions)(withRouter(MobileHeader));
