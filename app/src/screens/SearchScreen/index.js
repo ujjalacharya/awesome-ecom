26ddd6ffe7f93399927c8e0da9b1157240be985a
Ujjal Acharya <ujjal.acharya@amniltech.com>
2020-10-13 15:31:49 +0545

    app/show search suggestions from dummy data

--- app/src/screens/SearchScreen/index.js
+++ app/src/screens/SearchScreen/index.js
@@ -1,9 +1,15 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet, AsyncStorage } from "react-native";
 import { Searchbar, TouchableRipple, Button } from "react-native-paper";
 import Constants from "../../constants/Constants";
 
-import { AntDesign } from "@expo/vector-icons";
+import { AntDesign, Feather } from "@expo/vector-icons";
+
+const searchData = [
+  { id: 1, name: "One Plus 8T" },
+  { id: 2, name: "One on One" },
+  { id: 3, name: "Car One plus" },
+];
 
 const SeachScreen = (props) => {
   const [searchQuery, setSearchQuery] = useState("");
@@ -11,54 +17,47 @@ const SeachScreen = (props) => {
 
   useEffect(() => {
     fetchHistoryFromAsyncStorage();
-  }, [])
+  }, []);
 
-  const fetchHistoryFromAsyncStorage = async() => {
+  const fetchHistoryFromAsyncStorage = async () => {
     try {
-      let asyncItems =  await AsyncStorage.getItem(
-         '@uzzStore:history'
-       );
- 
-       if(asyncItems){
-         setHistory([...JSON.parse(asyncItems)])
-       }
-     } catch (error) {
-       // Error saving data
-     }
-  }
+      let asyncItems = await AsyncStorage.getItem("@uzzStore:history");
+
+      if (asyncItems) {
+        setHistory([...JSON.parse(asyncItems)]);
+      }
+    } catch (error) {
+      // Error saving data
+    }
+  };
 
   const onChangeSearch = (query) => setSearchQuery(query);
 
-  const handleGo = async() => {
-    let newItemsArr= [];
+  const handleGo = async () => {
+    let newItemsArr = [];
     try {
-     let asyncItems =  await AsyncStorage.getItem(
-        '@uzzStore:history'
-      );
+      let asyncItems = await AsyncStorage.getItem("@uzzStore:history");
 
-      if(asyncItems){
-        newItemsArr = [searchQuery, ...JSON.parse(asyncItems)]
-      }else{
+      if (asyncItems) {
+        newItemsArr = [searchQuery, ...JSON.parse(asyncItems)];
+      } else {
         newItemsArr.push(searchQuery);
       }
       await AsyncStorage.setItem(
-        '@uzzStore:history',
+        "@uzzStore:history",
         JSON.stringify(newItemsArr)
       );
     } catch (error) {
       // Error saving data
     }
-    props.navigation.navigate("Products")
-  }
+    props.navigation.navigate("Products");
+  };
 
-  const handleRemoveHistory = async() => {
-    await AsyncStorage.removeItem(
-      '@uzzStore:history'
-      , ()=>{
-        setHistory([])
-      }
-    );
-  }
+  const handleRemoveHistory = async () => {
+    await AsyncStorage.removeItem("@uzzStore:history", () => {
+      setHistory([]);
+    });
+  };
 
   return (
     <View style={styles.searchViewWrapper}>
@@ -84,28 +83,75 @@ const SeachScreen = (props) => {
           </TouchableRipple>
         </View>
       </View>
-      <View style={{ flex: 15, backgroundColor: Constants.headerTintColor }}>
-        <View
-          style={{ height: 30, width: "100%", margin: 5, flexDirection: "row" }}
-        >
-          <View style={{ flex: 0.8, justifyContent: "center" }}>
-            <Text>{"Search History"}</Text>
+      {searchQuery ? (
+        <View style={{ flex: 1, backgroundColor: Constants.headerTintColor }}>
+          {searchData.map((item) => {
+            return (
+              <View
+                style={{
+                  borderColor: "#000",
+                  borderBottomWidth: 1,
+                  height: 50,
+                  alignItems: "center",
+                  paddingLeft: 5,
+                  flexDirection: "row",
+                }}
+              >
+                <View style={{ flex: 0.9, marginLeft: 5 }}>
+                  <Text style={{ fontWeight: "bold" }}>{item.name}</Text>
+                </View>
+                <TouchableRipple onPress={() => setSearchQuery(item.name)}>
+                  <Feather name="arrow-up-left" size={20} color="gray" />
+                </TouchableRipple>
+              </View>
+            );
+          })}
+        </View>
+      ) : (
+        <View style={{ flex: 1, backgroundColor: Constants.headerTintColor }}>
+          <View
+            style={{
+              height: 30,
+              width: "100%",
+              margin: 5,
+              flexDirection: "row",
+            }}
+          >
+            <View style={{ flex: 0.8, justifyContent: "center" }}>
+              <Text>{"Search History"}</Text>
+            </View>
+            <View style={{ flex: 0.2 }}>
+              <Button onPress={handleRemoveHistory}>
+                <AntDesign name="delete" color="red" />
+              </Button>
+            </View>
           </View>
-          <View style={{ flex: 0.2 }}>
-            <Button onPress={handleRemoveHistory}>
-              <AntDesign name="delete" color="red" />
-            </Button>
+          <View
+            style={{
+              minHeight: 200,
+              flex: 1,
+              flexDirection: "row",
+              flexWrap: "wrap",
+            }}
+          >
+            {history.map((item) => (
+              <TouchableRipple
+                onPress={() => console.warn("history")}
+                style={{
+                  flexBasis: "28%",
+                  height: 40,
+                  backgroundColor: "#eff0f5",
+                  margin: 10,
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Text>{item}</Text>
+              </TouchableRipple>
+            ))}
           </View>
         </View>
-        <View style={{ minHeight: 200, flex: 1, flexDirection: "row", flexWrap: 'wrap' }}>
-          {history.map(item => (
-            <TouchableRipple onPress={()=> console.warn("history")} style={{flexBasis: '28%', height: 40, backgroundColor: "#eff0f5", margin: 10, justifyContent: "center", alignItems: "center"}}>
-              <Text>{item}</Text>
-            </TouchableRipple>
-           
-          ))}
-        </View>
-      </View>
+      )}
     </View>
   );
 };
