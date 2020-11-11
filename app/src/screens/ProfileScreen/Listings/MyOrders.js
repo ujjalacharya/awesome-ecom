3709f375e6d3d64ed17739fee52262517dd43845
Ujjal Acharya <ujjal.acharya@amniltech.com>
2020-11-11 14:27:36 +0545

    app/ make HOC for all listing screens

--- app/src/screens/ProfileScreen/Listings/MyOrders.js
+++ app/src/screens/ProfileScreen/Listings/MyOrders.js
@@ -1,30 +1,14 @@
 import React from "react";
-import { View, ScrollView } from "react-native";
-import { Appbar } from "react-native-paper";
-import Constants from "../../../constants/Constants";
+import { View } from "react-native";
+import ListingScreen from "../../../components/ListingScreen";
 import { productData } from "../../../utils/mock";
 import FeaturedProducts from "../../HomeScreen/FeaturedProducts";
 import SearchedSingleProduct from "../../ProductListScreen/SearchedSingleProduct";
-import { useNavigation } from "@react-navigation/native";
-
 
 const MyOrders = (props) => {
-    const navigation = useNavigation();
   return (
-    <ScrollView
-      contentContainerStyle={{ flexGrow: 1 }}
-      stickyHeaderIndices={[1]}
-      showsVerticalScrollIndicator={false}
-      showsHorizontalScrollIndicator={false}
-    >
-      <View>
-        <Appbar.Header statusBarHeight={0}>
-          <Appbar.BackAction
-            onPress={()=> navigation.goBack()}
-            color={Constants.headerTintColor}
-          />
-          <Appbar.Content title="My Orders" color={Constants.headerTintColor} />
-        </Appbar.Header>
+    <ListingScreen title="My Orders">
+      <>
         {productData.map((product, i) => (
           <SearchedSingleProduct
             product={product}
@@ -36,8 +20,8 @@ const MyOrders = (props) => {
         <View style={{ height: 200 }}>
           <FeaturedProducts title={"Recommended Products"} />
         </View>
-      </View>
-    </ScrollView>
+      </>
+    </ListingScreen>
   );
 };
 
