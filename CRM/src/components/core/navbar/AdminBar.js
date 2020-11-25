91822e79b6c17aa3f128bcb27a12b59708668b2e
AanandBhandari <aanandbhandari143@gmail.com>
2020-11-25 13:24:51 +0545

    CRM/refactor sidebar

--- CRM/src/components/core/navbar/AdminBar.js
+++ CRM/src/components/core/navbar/AdminBar.js
@@ -22,10 +22,11 @@ const AdminBar = ({}) => {
             key:'orders',
             icon: 'layout',
             main: 'Order',
-            sub: [ {
-                name: 'Manage Orders',
-                path: '/order'
-            }]
+            path:'/order',
+            // sub: [ {
+            //     name: 'Manage Orders',
+            //     path: '/order'
+            // }]
         }
     ],[])
 
