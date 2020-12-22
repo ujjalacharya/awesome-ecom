36627c1d26951e18a5e2888efe2fb6e1c1fd1cdc
Utsav170 <stha24@gmail.com>
2020-12-22 15:59:14 +0545

    fixes in server base url

--- client/utils/constants.js
+++ client/utils/constants.js
@@ -1,6 +1,6 @@
 export const KINDEEM_BASE_URL = 'http://157.245.106.101'
 
-export const BASE_URL = process.env.NODE_ENV === 'production' ? '' : process.env.SERVER_BASE_URL
+export const BASE_URL = process.env.NODE_ENV === 'production' ? 'http://157.245.106.101:3001' : process.env.SERVER_BASE_URL
 
 export const USER_AUTH_BASE_URL = `${BASE_URL}/api/user-auth`
 export const USER_BASE_URL = `${BASE_URL}/api/user`
