b8a7349bf1699cf1e2716b9395f8482248e7fa0d
Utsav170 <stha24@gmail.com>
2020-12-23 12:19:09 +0545

    fixes in product video modal

--- client/src/Includes/Details/Includes/ProductVideo.js
+++ client/src/Includes/Details/Includes/ProductVideo.js
@@ -2,13 +2,14 @@ import React from 'react'
 import ModalVideo from 'react-modal-video'
 
 const ProductVideo = (props) => {
+    let videoId = props.data?.videoURL[0].split('=').pop()
     return (
         <div>
             <ModalVideo 
                 channel='youtube' 
                 autoplay
                 isOpen={props.openVideo} 
-                videoId="L61p2uyiMSo" 
+                videoId={videoId}
                 onClose={props.onCloseVideo} 
             />
             {/* <ReactPlayer controls={true} url='https://www.youtube.com/watch?v=iBFDGKfY7xU&ab_channel=BestEverFoodReviewShow' /> */}
