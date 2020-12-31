92901ab61d49517576ce2099e84889cdf1e68adc
Utsav170 <stha24@gmail.com>
2020-12-31 13:16:11 +0545

    add link in banner

--- client/src/Components/Carousel.js
+++ client/src/Components/Carousel.js
@@ -1,11 +1,8 @@
 import React, { Component } from "react";
 import { Carousel, Button } from "antd";
 import { IMAGE_BASE_URL } from "../../utils/constants";
-
-// import Carousel from "nuka-carousel";
-
 class MainCarousel extends Component {
-  onChange = (a, b, c) => {};
+  onChange = (a, b, c) => { };
   render() {
     const settings = {
       dots: true,
@@ -15,17 +12,25 @@ class MainCarousel extends Component {
       slidesToScroll: 1,
       draggable: true,
     };
+    
     return (
       <Carousel {...settings}>
-          {this.props.data?.banners &&
-            this.props.data.banners.map((banner,i) => {
-              return (
-                <div className="img-text" key={i}>
-                  <div className="carousel-text"></div>
-                  <img src={IMAGE_BASE_URL+'/'+banner.bannerPhoto} alt="banner" />
-                </div>
-              );
-            })}
+        {this.props.data?.banners &&
+          this.props.data.banners.map((banner, i) => {
+            return (
+              banner.link ?
+                  <a href={banner.link} target="_blank" className="img-text" key={i}>
+                    <div className="carousel-text"></div>
+                    <img src={IMAGE_BASE_URL + '/' + banner.bannerPhoto} alt="banner" />
+                  </a>
+                 : (
+                  <div className="img-text" key={i}>
+                    <div className="carousel-text"></div>
+                    <img src={IMAGE_BASE_URL + '/' + banner.bannerPhoto} alt="banner" />
+                  </div>
+                )
+            );
+          })}
       </Carousel>
     );
   }
