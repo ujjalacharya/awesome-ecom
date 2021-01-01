22de7db2c5ad9eca718dd9b534b312c91dc878e7
Utsav170 <stha24@gmail.com>
2021-01-01 23:04:13 +0545

    client/product lists change to functional

--- client/src/Includes/Listing/ProductList.js
+++ client/src/Includes/Listing/ProductList.js
@@ -1,222 +1,214 @@
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
 import { Select, Row, Col } from "antd";
 import { AppstoreOutlined, BarsOutlined } from "@ant-design/icons";
 import _ from "lodash";
 
 //includes
 import ProductCard from "../../Components/Includes/ProductCard";
-import { getBrandOptions, getColorOptions } from "../../../utils/common";
-import { connect } from "react-redux";
+import { getBrandOptions, getColorOptions, previousQuery } from "../../../utils/common";
+import { connect, useDispatch, useSelector } from "react-redux";
 import actions from "../../../redux/actions";
 import { withRouter } from "next/router";
+import { SearchFilterSkeleton } from "../../../utils/skeletons";
 
 // Select Option
 const { Option } = Select;
 
-class ProductList extends Component {
-  state = {
-    min_price: "",
-    max_price: "",
-    sortBy: 'ace'
-  };
+const ProductList = (props) => {
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart)
+  const wishlist = useSelector((state) => state.wishlist)
+  
+  let [min_price, setMinPrice] = useState('')
+  let [max_price, setMaxPrice] = useState('')
+  let [searchFilters, setSearchFilters] = useState(SearchFilterSkeleton);
 
-  static getDerivedStateFromProps(nextProps, prevState) {
-    if (
-      nextProps.currentFilter.max_price !== prevState.max_price ||
-      nextProps.currentFilter.min_price !== prevState.min_price
-    ) {
-      let max_price = "";
-      let min_price = "";
-
-      max_price = nextProps.currentFilter.max_price;
-      min_price = nextProps.currentFilter.min_price;
-
-      return {
-        max_price,
-        min_price,
-      };
+  useEffect(() => {
+    if(max_price){
+      setMaxPrice(props.currentFilter.max_price)
     }
 
-    return null;
-  }
+    if(min_price){
+      setMinPrice(props.currentFilter.min_price)
+    }
+  }, [max_price, min_price])
+  
+  let { cate, slug } = props.router.query;
 
+  let prevAddToCartResp = previousQuery(cart.addToCartResp)
+  let prevRemoveFromCartResp = previousQuery(cart.removeFromCartResp)
 
-  componentDidUpdate(prevProps) {
-    let { cate, slug } = this.props.router.query
-    if (
-      this.props.cart.addToCartResp !== prevProps.cart.addToCartResp &&
-      this.props.cart.addToCartResp
-    ) {
-      this.props.getProductsByCategory(`?page=1&perPage=10&cat_id=${cate}&cat_slug=${slug}`);
+  let prevWishlistItemsResp = previousQuery(wishlist.wishlistItemsResp)
+  let prevRemoveFromWishlistResp = previousQuery(wishlist.removeFromWishlistResp)
+  
+  useEffect(() => {
+    if(cart.addToCartResp !== prevAddToCartResp && cart.addToCartResp){
+      dispatch(actions.getProductsByCategory(`?page=1&perPage=10&cat_id=${cate}&cat_slug=${slug}`))
+    }
+    
+    if(cart.removeFromCartResp !== prevRemoveFromCartResp && cart.removeFromCartResp){
+      dispatch(actions.getProductsByCategory(`?page=1&perPage=10&cat_id=${cate}&cat_slug=${slug}`))
+    }
+    
+    if(wishlist.wishlistItemsResp !== prevWishlistItemsResp && wishlist.wishlistItemsResp){
+      dispatch(actions.getProductsByCategory(`?page=1&perPage=10&cat_id=${cate}&cat_slug=${slug}`))
+    }
+    
+    if(wishlist.removeFromWishlistResp !== prevRemoveFromWishlistResp && wishlist.removeFromWishlistResp){
+      dispatch(actions.getProductsByCategory(`?page=1&perPage=10&cat_id=${cate}&cat_slug=${slug}`))
     }
 
-    if (
-      this.props.wishlist.wishlistItemsResp !==
-      prevProps.wishlist.wishlistItemsResp &&
-      this.props.wishlist.wishlistItemsResp
-    ) {
-      this.props.getProductsByCategory(`?page=1&perPage=10&cat_id=${cate}&cat_slug=${slug}`);
+  }, [cart, wishlist])
+  
+  useEffect(() => {
+    if(props.searchFilter){
+      setSearchFilters(props.searchFilter)
     }
+  }, [props.searchFilter])
 
-    if (
-      this.props.wishlist.removeFromWishlistResp !==
-      prevProps.wishlist.removeFromWishlistResp &&
-      this.props.wishlist.removeFromWishlistResp
-    ) {
-      this.props.getProductsByCategory(`?page=1&perPage=10&cat_id=${cate}&cat_slug=${slug}`);
-    }
-
-    if (
-      this.props.cart.removeFromCartResp !==
-      prevProps.cart.removeFromCartResp &&
-      this.props.cart.removeFromCartResp
-    ) {
-      this.props.getProductsByCategory(`?page=1&perPage=10&cat_id=${cate}&cat_slug=${slug}`);
-    }
-  }
-
-  render() {
-    const { data, searchFilter, currentFilter } = this.props;
-    let brandOptions = getBrandOptions(searchFilter);
-
-    return (
-      <div className="product-lists">
-        <div className="sorting-page">
-          <div className="view-sort">
-            <div className="type-icon">
-              <AppstoreOutlined className="list-icon" />
-              {/* <BarsOutlined className="list-icon" /> */}
-            </div>
-            <Select
-              style={{ width: 200 }}
-              placeholder="SORT BY"
-              className="sortyBy-select"
-              onChange={(val) => this.props.sortProducts(val)}
-              defaultValue="asc"
-            >
-              <Option value="asc">Ascending</Option>
-              <Option value="desc">Descending</Option>
-            </Select>
+  const { data, currentFilter } = props;
+  
+  let brandOptions = getBrandOptions(searchFilters);
+  // if(!data){
+  //   return <h1>Loading</h1>
+  // }
+  return (
+    <div className="product-lists">
+      <div className="sorting-page">
+        <div className="view-sort">
+          <div className="type-icon">
+            <AppstoreOutlined className="list-icon" />
+            {/* <BarsOutlined className="list-icon" /> */}
           </div>
-          <div className="page-status">
-            Page {this.props.currentPage} of{" "}
-            {Math.ceil(data && data.totalCount / this.props.perPage)}
-          </div>
+          <Select
+            style={{ width: 200 }}
+            placeholder="SORT BY"
+            className="sortyBy-select"
+            onChange={(val) => props.sortProducts(val)}
+            defaultValue="asc"
+          >
+            <Option value="asc">Ascending</Option>
+            <Option value="desc">Descending</Option>
+          </Select>
         </div>
-        {!_.isEmpty(currentFilter) && _.size(currentFilter) > 1 && this.props.filterApplied && (
-          <div className="filtered-by">
-            <span className="title">Filtered By:</span>
-            {currentFilter.brands &&
-              currentFilter.brands.length > 0 &&
-              currentFilter.brands.map((brand, i) => {
-                return (
-                  <>
-                    {brandOptions.map((allBrand) => {
-                      return (
-                        <>
-                          {brand === allBrand.value && (
-                            <span
-                              className="filter-tags"
-                              onClick={() => this.props.removeBrand(brand)}
-                              key={i}
-                            >
-                              Brand: {allBrand.label}
-                              <i className="fa fa-times" aria-hidden="true"></i>
-                            </span>
-                          )}
-                        </>
-                      );
-                    })}
-                  </>
-                );
-              })}
-
-            {currentFilter.colors &&
-              currentFilter.colors.length > 0 &&
-              currentFilter.colors.map((color, i) => {
-                return (
-                  <span
-                    className="filter-tags"
-                    onClick={() => this.props.removeColor(color)}
-                    key={i}
-                  >
-                    Color: {color}
-                    <i className="fa fa-times" aria-hidden="true"></i>
-                  </span>
-                );
-              })}
-
-            {currentFilter.ratings && (
-              <span
-                className="filter-tags"
-                onClick={() => this.props.removeRating(currentFilter.ratings)}
-              >
-                Rating: {currentFilter.ratings} And Up
-                <i className="fa fa-times" aria-hidden="true"></i>
-              </span>
-            )}
-
-            {(this.state.max_price || this.state.min_price) && (
-              <span
-                className="filter-tags"
-                onClick={() =>
-                  this.props.removePrice(
-                    this.state.min_price,
-                    this.state.max_price
-                  )
-                }
-              >
-                Price: {this.state.min_price}{" "}
-                {this.state.min_price && this.state.max_price && "-"}{" "}
-                {this.state.max_price}
-                <i className="fa fa-times" aria-hidden="true"></i>
-              </span>
-            )}
-
-
-            {!_.isEmpty(currentFilter.warranties) &&
-              currentFilter.warranties.map((warenty, i) => {
-                return (
-                  <span
-                    className="filter-tags"
-                    onClick={() => this.props.removeWarrenty(warenty)}
-                    key={i}
-                  >
-                    Warrenty: {warenty}
-                    <i className="fa fa-times" aria-hidden="true"></i>
-                  </span>
-                );
-              })}
-
-            {!_.isEmpty(currentFilter.sizes) && (
-              <span
-                className="filter-tags"
-                onClick={() => this.props.removeSize()}
-              >
-                Size: {currentFilter.sizes}
-                <i className="fa fa-times" aria-hidden="true"></i>
-              </span>
-            )}
-          </div>
-        )}
-        <div className="card-list">
-          {
-            data && data.products.length > 0 ?
-              (<Row gutter={30}>
-                {data.products.map((data, i) => {
-                  return (
-                    <Col lg={6} sm={12} xs={24} key={i}>
-                      <ProductCard data={data} />
-                    </Col>
-                  );
-                })}
-              </Row>)
-              : "No Products Available"}
-
+        <div className="page-status">
+          Page {props.currentPage} of{" "}
+          {Math.ceil(data && data.totalCount / props.perPage)}
         </div>
       </div>
-    );
-  }
+      {!_.isEmpty(currentFilter) && _.size(currentFilter) > 1 && props.filterApplied && (
+        <div className="filtered-by">
+          <span className="title">Filtered By:</span>
+          {currentFilter.brands &&
+            currentFilter.brands.length > 0 &&
+            currentFilter.brands.map((brand, i) => {
+              return (
+                <>
+                  {brandOptions.map((allBrand) => {
+                    return (
+                      <>
+                        {brand === allBrand.value && (
+                          <span
+                            className="filter-tags"
+                            onClick={() => props.removeBrand(brand)}
+                            key={i}
+                          >
+                            Brand: {allBrand.label}
+                            <i className="fa fa-times" aria-hidden="true"></i>
+                          </span>
+                        )}
+                      </>
+                    );
+                  })}
+                </>
+              );
+            })}
+
+          {currentFilter.colors &&
+            currentFilter.colors.length > 0 &&
+            currentFilter.colors.map((color, i) => {
+              return (
+                <span
+                  className="filter-tags"
+                  onClick={() => props.removeColor(color)}
+                  key={i}
+                >
+                  Color: {color}
+                  <i className="fa fa-times" aria-hidden="true"></i>
+                </span>
+              );
+            })}
+
+          {currentFilter.ratings && (
+            <span
+              className="filter-tags"
+              onClick={() => props.removeRating(currentFilter.ratings)}
+            >
+              Rating: {currentFilter.ratings} And Up
+              <i className="fa fa-times" aria-hidden="true"></i>
+            </span>
+          )}
+
+          {(max_price || min_price) && (
+            <span
+              className="filter-tags"
+              onClick={() =>
+                props.removePrice(
+                  min_price,
+                  max_price
+                )
+              }
+            >
+              Price: {min_price}{" "}
+              {min_price && max_price && "-"}{" "}
+              {max_price}
+              <i className="fa fa-times" aria-hidden="true"></i>
+            </span>
+          )}
+
+
+          {!_.isEmpty(currentFilter.warranties) &&
+            currentFilter.warranties.map((warenty, i) => {
+              return (
+                <span
+                  className="filter-tags"
+                  onClick={() => props.removeWarrenty(warenty)}
+                  key={i}
+                >
+                  Warrenty: {warenty}
+                  <i className="fa fa-times" aria-hidden="true"></i>
+                </span>
+              );
+            })}
+
+          {!_.isEmpty(currentFilter.sizes) && (
+            <span
+              className="filter-tags"
+              onClick={() => props.removeSize()}
+            >
+              Size: {currentFilter.sizes}
+              <i className="fa fa-times" aria-hidden="true"></i>
+            </span>
+          )}
+        </div>
+      )}
+      <div className="card-list">
+        {
+          data && data.products.length > 0 ?
+            (<Row gutter={30}>
+              {data.products.map((data, i) => {
+                return (
+                  <Col lg={6} sm={12} xs={24} key={i}>
+                    <ProductCard data={data} />
+                  </Col>
+                );
+              })}
+            </Row>)
+            : "No Products Available"}
+
+      </div>
+    </div>
+  );
 }
 
-export default connect((state) => state, actions)(withRouter(ProductList));
+export default withRouter(ProductList);
