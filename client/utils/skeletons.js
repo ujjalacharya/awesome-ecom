7dfaf4a577bc956ac35970c5a2426a03317f4d31
Utsav170 <stha24@gmail.com>
2021-01-01 17:25:56 +0545

    client/fixes in product details page data fetch client to server

---
+++ client/utils/skeletons.js
@@ -0,0 +1,78 @@
+export const productDetailSkeleton = {
+    category: [{
+        brands: [''],
+        slug: '',
+        _id: 1,
+        systemName: '',
+        displayName: '',
+        parent: {
+            brands: [''],
+            slug: '',
+            _id: "1",
+            systemName: '',
+            displayName: '',
+        }
+    }],
+    images: [{
+        _id: "1",
+        thumbnail: '/images/default-image.png',
+        medium: '/images/default-image.png',
+        large: '/images/default-image.png',
+    }],
+    size: [''],
+    color: [''],
+    weight: [''],
+    tags: [''],
+    discountRate: '',
+    isVerified: '',
+    isFeatured: '',
+    viewsCount: '',
+    noOfSoldOut: '',
+    slug: '',
+    remark: [''],
+    _id: '1',
+    model: '',
+    description: '',
+    name: '',
+    brand: {
+        slug: '',
+        _id: 1,
+        systemName: '',
+        displayName: ''
+    },
+    warranty: '',
+    return: '',
+    quantity: '',
+    soldBy: {
+        holidayMode: {
+            end: '',
+            start: ''
+        },
+        _id: "1",
+        address: '',
+        shopName: ''
+    },
+    updatedAt: '',
+    highlights: '',
+    totalRatingUsers: '',
+    averageRating: {
+        $numberDecimal: ""
+    },
+    price: {
+        $numberDecimal: ''
+    },
+    stars: {
+        fiveStars: 0,
+        fourStars: 0,
+        threeStars: 0,
+        twoStars: 0,
+        oneStars: 0,
+        averageStar: 0,
+        totalRatingUsers: 0
+    },
+    hasOnCart: null,
+    hasBought: null,
+    hasOnWishlist: null,
+    hasReviewed: null,
+    videoURL: [''],
+}
