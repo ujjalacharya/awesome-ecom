b0fce2d0e71a6707b66d480e05d553ddc6ac75b1
Utsav170 <stha24@gmail.com>
2021-01-02 00:29:41 +0545

    client/fixes in fetch data cart

--- client/pages/cart.js
+++ client/pages/cart.js
@@ -1,71 +1,96 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import { Row, Col } from "antd";
-import { connect } from "react-redux";
-import { withRouter } from "next/router";
+
+// redux
+import { useDispatch, useSelector } from "react-redux";
+import actions from "../redux/actions";
 
 //includes
 import CartItems from "../src/Includes/Cart/CartItems";
 import OrderSummary from "../src/Includes/Cart/OrderSummary";
 import Layout from "../src/Components/Layout";
+
+// utils
 import initialize from "../utils/initialize";
-import actions from "../redux/actions";
-import { getCookie } from "../utils/cookie";
+import { getCookieFromBrowser, getCookie } from "../utils/cookie";
 import { getUserInfo } from "../utils/common";
+import withPrivate from "../utils/auth/withPrivate";
 
-class Cart extends Component {
-  state = {
-    checkoutItems: []
+const Cart = (props) => {
+  const dispatch = useDispatch();
+  const cart = useSelector((state => state.cart))
+
+  let [checkoutItems, setCheckoutItems] = useState([])
+
+  const getCheckoutItems = (items) => {
+    setCheckoutItems(items)
   }
+  let loginToken = getCookieFromBrowser('token');
+  let userInfo = getUserInfo(loginToken);
 
-  static async getInitialProps(ctx) {
-    initialize(ctx);
-
-    const {
-      query: { slug },
-    } = ctx;
-
-    if (ctx.isServer) {
-      let loginToken = getCookie("token", ctx.req);
-      let userInfo = getUserInfo(loginToken);
-
+  useEffect(() => {
+    if(!props.isServer){
       if (userInfo?._id) {
-        await ctx.store.dispatch(
-          actions.getUserProfile(userInfo._id, ctx)
+        dispatch(
+          actions.getUserProfile(userInfo._id)
         );
       }
-
-      await ctx.store.dispatch(
-        actions.getCartProducts("page=1", ctx)
+  
+      dispatch(
+        actions.getCartProducts("page=1")
       );
     }
+  }, [])
+
+  return (
+    <Layout title="Cart">
+      <section className="cart-page">
+        <div className="container">
+          <Row>
+            <Col md={0} xs={0}></Col>
+            <Col md={16} xs={24}>
+              <CartItems 
+                getCheckoutItems={getCheckoutItems} 
+                cartData={cart.directBuyItems || cart.getCartProducts}
+                showCheckbox="" 
+              />
+            </Col>
+            <Col md={8} xs={24}>
+              <OrderSummary 
+                orderTxt="PROCCED TO CHECKOUT" 
+                checkoutItems={checkoutItems} 
+                diableOrderBtn={!checkoutItems.length && 'disableBtn'} 
+              />
+            </Col>
+            <Col md={0} xs={0}></Col>
+          </Row>
+        </div>
+      </section>
+    </Layout>
+  );
+}
+
+Cart.getInitialProps = async(ctx) => {
+  initialize(ctx);
+
+  if (ctx.isServer) {
+    let loginToken = getCookie("token", ctx.req);
+    let userInfo = getUserInfo(loginToken);
+
+    if (userInfo?._id) {
+      await ctx.store.dispatch(
+        actions.getUserProfile(userInfo._id, ctx)
+      );
+    }
+
+    await ctx.store.dispatch(
+      actions.getCartProducts("page=1", ctx)
+    );
   }
 
-  getCheckoutItems = (items) => {
-    this.setState({
-      checkoutItems: items
-    })
-  }
-  render() {
-    return (
-      <Layout title="Cart">
-        <section className="cart-page">
-          <div className="container">
-            <Row>
-              <Col md={0} xs={0}></Col>
-              <Col md={16} xs={24}>
-                <CartItems getCheckoutItems={this.getCheckoutItems} cartData={this.props.cart.directBuyItems || this.props.cart.getCartProducts}
-                  showCheckbox="" />
-              </Col>
-              <Col md={8} xs={24}>
-                <OrderSummary orderTxt="PROCCED TO CHECKOUT" checkoutItems={this.state.checkoutItems} diableOrderBtn={!this.state.checkoutItems.length && 'disableBtn'} />
-              </Col>
-              <Col md={0} xs={0}></Col>
-            </Row>
-          </div>
-        </section>
-      </Layout>
-    );
+  return{
+    isServer: ctx.isServer
   }
 }
 
-export default connect((state) => state)(Cart);
+export default withPrivate(Cart);
